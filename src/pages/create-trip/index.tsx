810cb357b1a0cb93a1bf5c855f7dc9ec292404ca
Luã <luacal@gmail.com>
2024-07-10 23:51:05 -0300

    Install Axios and integrating the trip creation with the backend API

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -5,6 +5,7 @@ import { ConfirmTripModal } from "./confirm-trip-modal";
 import { DestinationAndDateStep } from "./steps/destination-and-date-step";
 import { InviteGuestsStep } from "./steps/invite-guests-step";
 import { DateRange } from "react-day-picker";
+import { api } from "../../lib/axios";
 
 export function CreateTripPage() {
   const navigate = useNavigate();
@@ -22,7 +23,7 @@ export function CreateTripPage() {
 
   const [emailsToInvite, setEmailsToInvite] = useState([
     "person124@hotmail.com",
-    "jessica.white44@yahoo.com"
+    "jessica.white44@yahoo.com",
   ]);
 
   function openGuestsInput() {
@@ -49,14 +50,45 @@ export function CreateTripPage() {
     setIsConfirmTripModalOpen(false);
   }
 
-  function createTrip(event: FormEvent<HTMLFormElement>) {
+  async function createTrip(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    navigate("/trips/123");
-    console.log('destination', destination)
-    console.log('eventStartAndEndDates', eventStartAndEndDates)
-    console.log('emailsToInvite', emailsToInvite)
-    console.log('ownerName', ownerName)
-    console.log('ownerEmail', ownerEmail)
+    
+
+    console.log("destination", destination);
+    console.log("eventStartAndEndDates", eventStartAndEndDates);
+    console.log("emailsToInvite", emailsToInvite);
+    console.log("ownerName", ownerName);
+    console.log("ownerEmail", ownerEmail);
+
+    if(!destination) {
+      return;
+    }
+
+    if(!eventStartAndEndDates?.from || !eventStartAndEndDates?.to) {
+      return;
+    }
+
+    if (emailsToInvite.length === 0) {
+      return;
+    }
+
+    if (!ownerName || !ownerEmail) {
+      return;
+    }
+
+   const response = await api.post("/trips", {
+      destination: destination,
+      starts_at: eventStartAndEndDates.from,
+      ends_at: eventStartAndEndDates.to,
+      emails_to_invite: emailsToInvite,
+      owner_name: ownerName,
+      owner_email: ownerEmail
+    });
+
+    const {tripId} = response.data;
+
+    navigate(`/trips/${tripId}`);
+
   }
 
   function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
